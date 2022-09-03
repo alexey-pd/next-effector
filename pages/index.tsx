19eaa482a93117f511eab71eceec2ce3aa4e671b
Alexey Pudnikov <alexey.pudnikov@yandex.ru>
2022-09-03 08:22:33 +0000

    Update package.json and index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
-import {fork, serialize, allSettled} from 'effector'
-import {useStore, useList, useStoreMap} from 'effector-react'
-import {queryAllChatsFx, queryCurrentUserFx} from '../app/query'
-import {$chats, $currentUser, $currentUserName} from '../app/state'
-import {$unreadCount} from '../app/unreadCount'
+import { fork, serialize, allSettled } from "effector";
+import { useUnit, useList, useStoreMap } from "effector-react";
+import { queryAllChatsFx, queryCurrentUserFx } from "../app/query";
+import { $chats, $currentUser, $currentUserName } from "../app/state";
+import { $unreadCount } from "../app/unreadCount";
 
-import {ChatLinkView, IndexPageView} from '../app/view'
+import { ChatLinkView, IndexPageView } from "../app/view";
 
 export default function Index() {
-  const userName = useStore($currentUserName)
+  const [userName] = useUnit([$currentUserName]);
   return (
     <IndexPageView userName={userName}>
       {useList($chats, {
-        getKey: ({id}) => id,
-        fn: ({name, id, icon, messages}) => {
+        getKey: ({ id }) => id,
+        fn: ({ name, id, icon, messages }) => {
           const unread = useStoreMap({
             store: $unreadCount,
             keys: [id, messages],
             fn(items) {
-              const item = items.find((item) => item.chatID === id)
-              if (!item) return messages.length
-              return item.count
+              const item = items.find((item) => item.chatID === id);
+              if (!item) return messages.length;
+              return item.count;
             },
-          })
+          });
           return (
             <ChatLinkView
               url={`/chat/${id}`}
@@ -30,20 +30,20 @@ export default function Index() {
               unread={unread}
               name={name}
             />
-          )
+          );
         },
       })}
     </IndexPageView>
-  )
+  );
 }
 
 export async function getStaticProps({}) {
-  const scope = fork({values: [[$currentUser, 1]]})
-  await allSettled(queryAllChatsFx, {scope})
-  await allSettled(queryCurrentUserFx, {scope})
+  const scope = fork({ values: [[$currentUser, 1]] });
+  await allSettled(queryAllChatsFx, { scope });
+  await allSettled(queryCurrentUserFx, { scope });
   return {
     props: {
       initialState: serialize(scope),
     },
-  }
+  };
 }
